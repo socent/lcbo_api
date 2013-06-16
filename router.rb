2be4da628e2652329d794bf665705034920dd8c9
socent <brad_wilcox@me.com>
2013-06-16 12:12:34 -0400

    initial commit

---
+++ router.rb
@@ -0,0 +1,24 @@
+require_relative 'html_generator'
+
+if ARGV.empty?
+  puts "Usage: ruby router.rb [action]"
+else
+  action = ARGV[0]
+  generator = HtmlGenerator.new
+
+  if 
+    action == "index"
+    search_text = ARGV[1]
+    generator.index(search_text)
+  elsif 
+    action == "show"
+    inventory_id = ARGV[1]
+    if inventory_id.nil?
+      puts "Usage: ruby router.rb show [inventory_id]"
+    else
+      generator.show(id)
+    end
+  else
+    puts "Unknown action #{action}. Use index or show."
+  end
+end
