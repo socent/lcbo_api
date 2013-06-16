09347c1866637d1c69193d6193a1586f6b08e5c4
socent <brad_wilcox@me.com>
2013-06-16 12:14:28 -0400

    refactor

--- router.rb
+++ router.rb
@@ -2,6 +2,7 @@ require_relative 'html_generator'
 
 if ARGV.empty?
   puts "Usage: ruby router.rb [action]"
+
 else
   action = ARGV[0]
   generator = HtmlGenerator.new
@@ -10,15 +11,20 @@ else
     action == "index"
     search_text = ARGV[1]
     generator.index(search_text)
+
   elsif 
     action == "show"
     inventory_id = ARGV[1]
+
     if inventory_id.nil?
       puts "Usage: ruby router.rb show [inventory_id]"
+
     else
       generator.show(id)
     end
+
   else
     puts "Unknown action #{action}. Use index or show."
   end
+
 end
